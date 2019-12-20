a64df4c18b58a365d07c1ae41b7be00ad677e4ed
sonnynajar <sonny.najar@gmail.com>
2019-12-20 07:40:19 -0800

    Update examples.rkt

--- cutscene/examples.rkt
+++ cutscene/examples.rkt
@@ -8,11 +8,67 @@
   cutscene hello-world-1
   (story-cutscene))
 
-
-; ----------------- TEXT MOVEMENT
+; ----------------- TEXT
 
 (define-example-code
   cutscene text-1
+
+  (story-cutscene
+   (page
+    (make-text "BIG text"
+               #:scale 5)
+    (make-text "Purple text"
+               #:color 'purple))))
+
+(define-example-code
+  cutscene text-2
+
+  (story-cutscene
+   (page
+    (make-text "Red text"
+               #:color 'red)
+    (make-text "Italicized text"
+               #:font-style 'italic))))
+
+(define-example-code
+  cutscene text-3
+
+  (story-cutscene
+   (page
+    (make-text "This font is slanted and small"
+               #:font-style 'slant
+               #:scale 0.3))))
+
+; ----------------- PAGES
+
+(define-example-code
+  cutscene page-1
+
+  (story-cutscene
+   (page #:bg-color 'orange
+         "The background"
+         "is orange.")))
+
+(define-example-code
+  cutscene page-2
+
+  (story-cutscene
+   (page #:border-color 'blue
+         "The border"
+         "is blue.")))
+
+(define-example-code
+  cutscene page-3
+
+  (story-cutscene
+   (page #:line-padding 20
+         "The space between"
+         "the lines is larger.")))
+
+; ----------------- CONTENT MODE
+
+(define-example-code
+  cutscene mode-1
 
   (story-cutscene
    (page #:mode 'still
@@ -20,7 +76,7 @@
   )
 
 (define-example-code
-  cutscene text-2
+  cutscene mode-2
 
   (story-cutscene
    (page #:mode 'scroll-right-from-center
@@ -30,7 +86,7 @@
   )
 
 (define-example-code
-  cutscene text-3
+  cutscene mode-3
 
   (story-cutscene
    (page #:mode 'scroll-down-from-center
